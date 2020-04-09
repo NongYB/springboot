977068a8a5cdc89d9e3b3c5cab4f80d08af1d0d6
NongYB <1369356538@qq.com>
2020-04-09 17:29:56 +0800

    第一次提交

---
+++ src/main/java/com/nong/githubstudy/GithubStudyApplication.java
@@ -0,0 +1,13 @@
+package com.nong.githubstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubStudyApplication.class, args);
+    }
+
+}
